6274e8b1d489b687ae6ad5ee36e0d03c79a01d58
Dankir30 <kirrdann@gmail.com>
2023-04-19 16:21:03 +0300

    third homework/models, migrations

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
+class Topic(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    user = models.ManyToManyField(User)
+
+    def __str__(self):
+        return self.title
+
+
+class BlogPost(models.Model):
+    slug_url = models.SlugField(max_length=50, null=True)
+    title = models.CharField(max_length=100)
+    content = models.TextField(null=True, blank=True)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.ManyToManyField(Topic)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    content = models.TextField(null=True, blank=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
 # Create your models here.
